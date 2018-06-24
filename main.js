abd03083e66a8cc5be5f5cf8464612679010b5f9
mdroberts74 <mikedroberts88@gmail.com>
2018-06-24 18:43:35 -0500

    I changed the const to let in the for loop.

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "
 let y = 1
 
 for (let x = 1; y < cookies.length; x++) {
-    const currentCookie = cookies[y]
+    let currentCookie = cookies[y]
     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
 }
+
+//I chanved the const currentCookie to let.
